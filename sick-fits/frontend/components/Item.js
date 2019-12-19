93363a01f092b4f9b6b362429ff25e17ea69cccc
Siddharth Venkatesh <siddhuv93@gmail.com>
2019-12-19 12:46:47 +0530

    items

---
+++ sick-fits/frontend/components/Item.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Item extends Component {
+    static propTypes =  {
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Item.propTypes = {
+
+};
+
+export default Item;
