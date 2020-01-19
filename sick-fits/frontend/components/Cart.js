c6a6f84ae23670fe9a3ff5c0d83ab0f32cb246a6
Siddharth Venkatesh <siddhuv93@gmail.com>
2020-01-20 00:31:15 +0530

    Search

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -5,6 +5,7 @@ import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
 import User from './User';
 import CartItem from './CartItem';
 import calcTotal from '../lib/calcTotalPrice';
@@ -20,43 +21,41 @@ const TOGGLE_CART_MUTATION = gql`
     mutation TOGGLE_CART_MUTATION {
         toggleCart @client
     }
-`
+`;
+
+const Composed = adopt({
+    user: ({ render }) => <User>{render}</User>,
+    toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+    localState: ({ render })=> <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+});
 
 const Cart = (props) => {
     return (
-        <User>
-            {({ data: { me }}) => {
-                if(!me) return null;
-                return (
-                    <Mutation mutation={TOGGLE_CART_MUTATION}>
-                        {
-                            (toggleCart) => (
-                                <Query query={LOCAL_STATE_QUERY}>
-                                    {
-                                        ({data}) => (
-                                            <CartStyles open={data.cartOpen}>
-                                                <header>
-                                                    <CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
-                                                    <Supreme>{me.name}'s cart</Supreme>
-                                                    <p>You have {me.cart.length} items in your cart</p>
-                                                </header>
-                                                <React.Fragment>
-                                                    {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem}/>)}
-                                                </React.Fragment>
-                                                <footer>
-                                                    <p>{formatMoney(calcTotal(me.cart))}</p>
-                                                    <SickButton>Checkout</SickButton>
-                                                </footer>
-                                            </CartStyles>
-                                        )
-                                    }
-                                </Query>
-                            )
-                        }
-                    </Mutation>
-                )
-            }}
-        </User>
+        <Composed>
+            {
+                ({user, toggleCart, localState}) => {
+                    const me = user.data.me;
+                    if(!me) return null;
+                    return (
+                        <CartStyles open={localState.data.cartOpen}>
+                            <header>
+                                <CloseButton onClick={toggleCart} title="close">&times;</CloseButton>
+                                <Supreme>{me.name}'s cart</Supreme>
+                                <p>You have {me.cart.length} items in your cart</p>
+                            </header>
+                            <React.Fragment>
+                                {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem}/>)}
+                            </React.Fragment>
+                            <footer>
+                                <p>{formatMoney(calcTotal(me.cart))}</p>
+                                <SickButton>Checkout</SickButton>
+                            </footer>
+                        </CartStyles>
+                                        
+                    )
+                }
+            }
+        </Composed>
         
     )
 }
